355d4e54d67376c2f600a3b6b06e73fed3e60895
Adercraft-R <adercraft39@gmail.com>
2026-02-20 23:11:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 const path = require("path");
